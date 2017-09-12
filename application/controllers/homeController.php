ff204e557db8600b1fcb30c8e6c187cce557076e
Admin jean.chagas.kinghost.net <admin@jean.chagas.kinghost.net>
2017-09-12 15:04:01 -0300

    Agora vai - teste na home

--- application/controllers/homeController.php
+++ application/controllers/homeController.php
@@ -1,1 +1,82 @@
-// gran controler - standby
+<?php 
+
+	function __autoload($file){
+        
+        if(file_exists(MODELS.$file.".class.php")){
+            
+            require_once(MODELS.$file.".class.php");
+
+	    }elseif(file_exists(CONTROLLERS.$file.".php")){
+            
+            require_once(CONTROLLERS.$file.".php");
+            
+        }else{            
+           
+            require_once(ERROS."404.php");
+            exit();
+            
+        }
+        
+    }  
+
+    $aux = (isset ($_GET['url']) ? $_GET['url'] : 'index');
+
+    $lista = array();
+    $lista = explode('/', $aux);
+
+    //print_r($lista);
+
+    switch ($lista[0]){
+
+    		case "create":
+
+    				$dados = array(
+    					'numero'   => '001',
+    					'apelido'  => '"primeiro"',
+    					'projetor' => '0'
+    				);
+
+                    $sala = new Sala();
+                    $sala->create($dados);
+                   
+                    break;
+
+            case "read":
+                    $sala = new Sala();
+                    print_r($sala->read());
+                   
+                    break;
+
+            case "update":
+            		$where = array(
+            			'id' => '3'
+            		);
+
+
+            		$dados = array(
+    					'numero'   => '101',
+    					'apelido'  => '"Segundo"',
+    					'projetor' => '1'
+    				);
+
+                    $sala = new Sala();
+                    $sala->update($dados, $where);
+                   
+                    break;
+
+            case "delete":
+
+            		$where = array(
+            			'id' => '3'
+            		);
+
+                    $sala = new Sala();
+                    print_r($sala->delete($where));
+                   
+                    break;
+
+
+
+    }
+
+ ?>
