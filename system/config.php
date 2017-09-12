373364e5b05d3bc720a69b719a329d9d1aa8c0e0
Admin jean.chagas.kinghost.net <admin@jean.chagas.kinghost.net>
2017-09-12 10:14:36 -0300

    arquivo de constantes

---
+++ system/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    define('_DOMAIN', 'http://' . $_SERVER['HTTP_HOST']);        
+    define('CONTROLLERS', 'app/controllers/'); 
+    define('MODELS', 'application/models/'); 
+    define('VIEWS', 'app/views/'); 
+    define('DB_HOST', 'mysql.jean.chagas.kinghost.net'); 
+    define('DB_NAME', 'jean01'); 
+    define('DB_USER', 'jean01'); 
+    define('DB_PASS', 'doscohab03');   
+    define('DB_CHARSET', 'SET NAMES utf8');
+    define("MYSQL_CONN_ERROR", "<center>Erro ao efetuar conexão com o bando de dados <b>".DB_HOST."</b>, entre em contato com o administrador do sistema. ");
+   
+
