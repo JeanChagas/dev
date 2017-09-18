f6c1684753fa63007a90c6d044623a09be606706
Admin jean.chagas.kinghost.net <admin@jean.chagas.kinghost.net>
2017-09-18 10:01:22 -0300

    Conexão com a base

--- system/config.php
+++ system/config.php
@@ -5,10 +5,10 @@
     define('MODELS', 'application/models/'); 
     define('VIEWS', 'application/views/'); 
     define('ERROS', 'assets/erros/');
-    define('DB_HOST', 'mysql.jean.chagas.kinghost.net'); 
-    define('DB_NAME', 'jean01'); 
-    define('DB_USER', 'jean01'); 
-    define('DB_PASS', 'doscohab03');   
+    define('DB_HOST', 'hostaqui'); 
+    define('DB_NAME', 'baseaqui'); 
+    define('DB_USER', 'usuarioaqui'); 
+    define('DB_PASS', 'senhaaqui');   
     define('DB_CHARSET', 'SET NAMES utf8');
     define("MYSQL_CONN_ERROR", "<center>Erro ao efetuar conexão com o bando de dados <b>".DB_HOST."</b>, entre em contato com o administrador do sistema. ");
    
